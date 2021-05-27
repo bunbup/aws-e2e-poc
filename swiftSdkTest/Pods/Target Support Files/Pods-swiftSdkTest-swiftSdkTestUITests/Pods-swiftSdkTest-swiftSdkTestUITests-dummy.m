7dbc728e06818e9d60876083c777d64a1017bc4c
Lukasz Klich <klich.lukasz@gmail.com>
2021-05-27 16:00:03 +0200

    Simple swift app

---
+++ swiftSdkTest/Pods/Target Support Files/Pods-swiftSdkTest-swiftSdkTestUITests/Pods-swiftSdkTest-swiftSdkTestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_swiftSdkTest_swiftSdkTestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_swiftSdkTest_swiftSdkTestUITests
+@end
---
+++ swiftSdkTest/Pods/Target Support Files/Pods-swiftSdkTest/Pods-swiftSdkTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_swiftSdkTest : NSObject
+@end
+@implementation PodsDummy_Pods_swiftSdkTest
+@end
