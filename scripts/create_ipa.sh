7c07b6510f3137f998bdd08d51dd63790dac1d68
Lukasz Klich <klich.lukasz@gmail.com>
2021-05-27 16:00:03 +0200

    Simple swift app

---
+++ scripts/create_ipa.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+set -o xtrace
+
+PROJECT=swiftSdkTest
+CURRENT_DIR=`pwd`
+TMPDIR=`mktemp -d /tmp/${PROJECT}.XXX` || exit 1
+ARCHIVE_PATH=$TMPDIR/${PROJECT}.xcarchive
+EXPORT_PATH=$TMPDIR/Payload # DIRECTORY
+xcodebuild -workspace ./${PROJECT}.xcworkspace -scheme ${PROJECT} archive -archivePath $ARCHIVE_PATH -verbose
+mkdir $EXPORT_PATH
+cp -r $ARCHIVE_PATH/Products/Applications/$PROJECT.app $EXPORT_PATH/Payload
+cd $EXPORT_PATH && zip -r -X "../Payload.ipa" .
+cd $CURRENT_DIR 
+mv $TMPDIR/Payload.ipa  .
