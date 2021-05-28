8467d7c0882360dcc5fb5d8d14777261aa96080d
Lukasz Klich <klich.lukasz@gmail.com>
2021-05-28 17:53:10 +0200

    With mobileprovision

--- scripts/decrypt_secrets.sh
+++ scripts/decrypt_secrets.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -eo pipefail
 
+gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASS" --output ./.github/secrets/6e742bf5-5ba7-4b72-9204-9c972c7477ee.mobileprovision ./.github/secrets/6e742bf5-5ba7-4b72-9204-9c972c7477ee.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASS" --output ./.github/secrets/cert.p12 ./.github/secrets/cert.p12.gpg
+
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+
+cp ./.github/secrets/6e742bf5-5ba7-4b72-9204-9c972c7477ee.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
 
 security create-keychain -p "" build.keychain
 security import ./.github/secrets/cert.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$GPG_PASS" -A
