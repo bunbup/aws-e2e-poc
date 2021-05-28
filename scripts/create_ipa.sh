3dde50b8597ba84e5f23df594ffbbfe8c8be5191
Lukasz Klich <klich.lukasz@gmail.com>
2021-05-28 16:38:07 +0200

    Allow automatic signing? I have no idea what I'm doing

--- scripts/create_ipa.sh
+++ scripts/create_ipa.sh
@@ -6,7 +6,7 @@ CURRENT_DIR=`pwd`
 TMPDIR=`mktemp -d /tmp/${PROJECT}.XXX` || exit 1
 ARCHIVE_PATH=$TMPDIR/${PROJECT}.xcarchive
 EXPORT_PATH=$TMPDIR/Payload # DIRECTORY
-xcodebuild -workspace ./${PROJECT}.xcworkspace -scheme ${PROJECT} archive -archivePath $ARCHIVE_PATH -verbose
+xcodebuild -workspace ./${PROJECT}.xcworkspace -allowProvisioningUpdates -scheme ${PROJECT} archive -archivePath $ARCHIVE_PATH -verbose
 mkdir $EXPORT_PATH
 cp -r $ARCHIVE_PATH/Products/Applications/$PROJECT.app $EXPORT_PATH/Payload
 cd $EXPORT_PATH && zip -r -X "../Payload.ipa" .
