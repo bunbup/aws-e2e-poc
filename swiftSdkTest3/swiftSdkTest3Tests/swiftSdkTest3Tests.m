23cca81784897947231946dc576c9be21a4fcd11
Lukasz Klich <klich.lukasz@gmail.com>
2021-06-10 14:53:14 +0200

    Use PubNub in test

--- swiftSdkTest3/swiftSdkTest3Tests/swiftSdkTest3Tests.m
+++ swiftSdkTest3/swiftSdkTest3Tests/swiftSdkTest3Tests.m
@@ -24,15 +24,52 @@
     // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
+- (void)waitToCompleteIn:(NSTimeInterval)delay
+               codeBlock:(void(^)(dispatch_block_t handler))codeBlock {
+    
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+    __block BOOL handlerCalled = NO;
+    
+    codeBlock(^{
+        if (!handlerCalled) {
+            handlerCalled = YES;
+            dispatch_semaphore_signal(semaphore);
+        }
+    });
+    
+    dispatch_time_t timeout = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delay * NSEC_PER_SEC));
+    dispatch_semaphore_wait(semaphore, timeout);
+    
+    XCTAssertTrue(handlerCalled);
+}
+
+
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results
+    NSString *pubKey = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"PUBNUB_PUB_KEY"];
+    NSString *subKey = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"PUBNUB_SUB_KEY"];
     XCTAssertTrue([[[NSBundle mainBundle] objectForInfoDictionaryKey:@"PUBNUB_PUB_KEY"] length] != 0);
     XCTAssertTrue([[[NSBundle mainBundle] objectForInfoDictionaryKey:@"PUBNUB_SUB_KEY"] length] != 0);
-    PNConfiguration *pnconfig = [PNConfiguration configurationWithPublishKey:@"myPublishKey"
-                                                            subscribeKey:@"mySubscribeKey"];
-    pnconfig.uuid = @"ReplaceWithYourClientIdentifier";
-    PubNub *client = [PubNub clientWithConfiguration:pnconfig];
+    PNConfiguration *pnconfig = [PNConfiguration configurationWithPublishKey:pubKey
+                                                            subscribeKey:subKey];
+    dispatch_queue_t callbackQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    PubNub *client = [PubNub clientWithConfiguration:pnconfig callbackQueue:callbackQueue];
+    client.logger.enabled = YES;
+    [client.logger setLogLevel:PNVerboseLogLevel];
+    NSTimeInterval testCompletionDelay = 20.0;
+    
+    [self waitToCompleteIn:testCompletionDelay codeBlock:^(dispatch_block_t handler) {
+        [client publish: @{ @"entry": @"entry" } toChannel:@"test-channel"
+              withCompletion:^(PNPublishStatus *status) {
+
+            XCTAssertFalse(status.isError);
+            XCTAssertNotNil(status.data.timetoken);
+            XCTAssertEqual(status.statusCode, 200);
+            
+            handler();
+        }];
+    }];
 }
 
 - (void)testPerformanceExample {
